d54be61115ae68dc4e04ba6215f97795f2a1f6cb
Adnan shaikh <adnan12042006@gmail.com>
2025-09-21 18:33:12 +0530

    Just changed a bit of index.js in backend folder and also in the last commit created a user schema in models.js folder that contains users in collection

--- job-portal/backend/index.js
+++ job-portal/backend/index.js
@@ -14,10 +14,10 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("✅ MongoDB connected");
     // Start server only after DB connects
     app.listen(process.env.PORT, () => {
-      console.log(`🚀 Server running on port ${process.env.PORT}`);
+      console.log(`Server running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
-    console.error("❌ MongoDB connection error:", err);
+    console.error(" MongoDB connection error:", err);
   });
 
